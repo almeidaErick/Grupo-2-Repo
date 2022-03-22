21e97138426d9cd3a3021cc31a9a1aec5cabb0c1
StevStrange <tivisor@hotmail.com>
2022-03-22 14:26:47 -0500

    Modificación funcion str

--- productInfo.py
+++ productInfo.py
@@ -1,12 +1,21 @@
+import requests
 class Product:
     def __init__(self, product, price,search):
         self.product = product
         self.price = price
 
     def __str__(self):
-        # complete this
-        # STEVEN
-        return "Hola"
+        url=f"https://api.wallapop.com/api/v3/general/search?keywords={self.search}%20&category_ids=12900&filters_source=seo_landing&longitude=-3.69196&latitude=40.41956&order_by=closest"
+        datos = requests.get(url)
+        lista=[]
+        for elementos in datos.json().get("search_objects"):
+            diccionario = {"Title": elementos.get("title")+"\n", "Description": elementos.get("description")+"\n",
+                    "Distance": elementos.get("distance")+"\n", "Currency": elementos.get("currency")+"\n",
+                    "Price": elementos.get("price")+"\n\n"
+                    }
+            lista.append(diccionario)
+
+        return str(lista)
 
     def to_dict(self):
         # complete this
