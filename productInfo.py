c5e05469909083235809ccaab857c5b7cd8c7c5a
StevStrange <tivisor@hotmail.com>
2022-03-22 15:29:55 -0500

    Modificación funcion str1

--- productInfo.py
+++ productInfo.py
@@ -7,17 +7,8 @@ class Product:
 
 
     def __str__(self, search):
-        url=f"https://api.wallapop.com/api/v3/general/search?keywords={self.search}%20&category_ids=12900&filters_source=seo_landing&longitude=-3.69196&latitude=40.41956&order_by=closest"
-        datos = requests.get(url)
-        lista=[]
-        for elementos in datos.json().get("search_objects"):
-            diccionario = {"Title": elementos.get("title")+"\n", "Description": elementos.get("description")+"\n",
-                    "Distance": elementos.get("distance")+"\n", "Currency": elementos.get("currency")+"\n",
-                    "Price": elementos.get("price")+"\n\n"
-                    }
-            lista.append(diccionario)
 
-        return str(lista)
+        return "Hola"
 
     def to_dict(self):
         # complete this
