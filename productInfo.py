8ba982af6083e2809b9dea65fbf8fac290f553db
StevStrange <tivisor@hotmail.com>
2022-03-22 21:47:49 -0500

    Creacion __str__completo

--- productInfo.py
+++ productInfo.py
@@ -1,12 +1,14 @@
 class Product:
 
-    def __init__(self, product, price):
+    def __init__(self, product, price, description,currency):
         self.product = product
         self.price = price
+        self.description = description
+        self.currency = currency
 
     def __str__(self):
         # STEVEN
-        return f"producto:{self.product}, precio:{self.price}"
+        return f"producto:{self.product}, precio:{self.price}, descripcion:{self.description}, currency:{self.currency}"
 
     def to_dict(self):
         # complete this
